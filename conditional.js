299e46a02882e0a6d34a8a3d3116b1a58954d93b
isha-web-dev <ishachouhan808@123>
2025-05-12 20:02:24 +0530

    first

---
+++ conditional.js
@@ -0,0 +1,36 @@
+// var n= parseInt(prompt("enter the number"))
+// if(n%2==0){
+//     alert("even")
+// }
+// else{
+//     alert("odd")
+// }
+
+
+// var n1=parseInt(prompt("enter the 1 number"))
+// var n2=parseInt(prompt("enter the 2 number"))
+// if(n1>n2){
+//     alert("greater")
+// }
+// else{
+//     alert("not greater")
+// }
+
+
+// var a=parseInt(prompt("enter the number"))
+// if(a%2==0){
+//     alert("positive")
+// }
+// else if(a%1==0){
+//     alert("negative")
+// }
+// else{
+//     alert("neutral")
+// }
+var a=1;
+do{
+    console.log(a)
+    a++;
+}
+
+while(a<=10)
