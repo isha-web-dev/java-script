2b3826f84990a08ec51507fd750e5b5a9dd103c5
isha-web-dev <ishachouhan808@123>
2025-05-25 19:42:03 +0530

    third

---
+++ 4 types function.js
@@ -0,0 +1,38 @@
+//no arg and no return
+// function fun(){
+//     console.log("hi")
+// }
+// fun()
+
+//with arg and no return
+// function Demo(a)
+// {
+//     console.log(a)
+// }
+// Demo("isha")
+
+//wap to print add two number with arg and no return
+// function Demo(a,b)
+// {
+//     let sum=a+b
+
+//     console.log(sum)
+// }
+//  let a= parseInt(prompt("enter the number a"))
+//     let b=parseInt(prompt("enter the number b"))
+   
+// Demo(a,b)
+
+
+ //with arg and with return
+//function Demo (a){
+    //return(a+a)
+//}
+//2 ways
+//let num=Demo(10)
+//console.log(num)
+//1 ways
+//console.log(Demo(10))
+
+
+
---
+++ array.js
@@ -0,0 +1,34 @@
+// let ar = []
+// for(let a=0; a<5; a++){
+//     ar[a]=prompt("Enter elements: ")
+// }
+// console.log(ar);
+// for(let k in ar){
+//     console.log(k);
+// }
+
+// arr = [11,"isha",78]
+// for(let val of arr){
+//     console.log(val)
+// }
+//for(let a=0;a<4;a++){
+   // console.log(arr[a]);
+//}
+
+
+//array elements
+
+let stu = ["A",81,"Bhopal",5.8,11,22,33,44,55,56,77]
+// console.log(stu);
+stu.push("Cybrom") //adds elements in the array to the last
+console.log(stu);
+// stu.pop() //deletes element from the last
+// console.log(stu);
+// stu.unshift(1,"0134CS67UUNI") //adds element to the first position
+// console.log(stu);
+// stu.shift() //deletes element from the first
+// console.log(stu);
+// let a = stu.slice(1,6);//gives some part of array
+// console.log(a);
+stu.splice(1,2,"isha")
+console.log(stu);
